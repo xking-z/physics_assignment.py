c501b33928f92df525322269b9b15476b32c94c2
vershimaimengeri@gmail.com <vershimaimengeri@gmail.com>
2024-12-30 14:55:36 +0100

    initial commit

---
+++ physics.py
@@ -0,0 +1,29 @@
+
+def a():
+    #area = "length * breadth
+    length=float(input("what is the length?"))
+    breadth=float(input(" what is the breadth?"))
+    print(length*breadth)
+def b():
+    #velocity = "displacement / time"
+    displacement=float(input("what is the displacement?"))
+    time=float(input("what is the time?"))
+    print(displacement/time)
+def c():
+    #force = "mass * acceleration"
+    mass=float(input("what is the mass?"))
+    acceleration=float(input("what is the acceleration?"))
+    print(mass*acceleration)
+def d():
+    #momentum = "mass * velocity"
+    mass=float(input("what is the mass?"))
+    velocity=float(input("what is the velocity?"))
+    print(mass*velocity)
+def e():
+    #volume = "length * breadth * height"
+    length=float(input("what is the length?"))
+    breadth=float(input("what is the breadth?"))
+    height=float(input("what is the height?"))
+    print(length*breadth*height)
+a();
+
